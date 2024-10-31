f0ecfca06928381746e6a047c95741cfb67e9522
Darkchanze <nico.wittemann@t-online.de>
2024-10-31 13:48:53 +0100

    access data from json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,1 +1,31 @@
-print("animals")
+import json
+from operator import ifloordiv
+from plistlib import loads
+
+ANIMALS_FILE = "animals_data.json"
+
+def load_data(ANIMALS_FILE):
+    with open(ANIMALS_FILE, "r") as handle:
+        return json.load(handle)
+
+
+def print_data(animals_data):
+    for animal in animals_data:
+        if "name" in animal:
+            print(f"Name: {animal["name"]}")
+        if "diet" in animal["characteristics"]:
+            print(f"Diet: {animal["characteristics"]["diet"]}")
+        if "locations" in animal:
+            print(f"Location: {animal["locations"][0]}")
+        if "type" in animal["characteristics"]:
+            print(f"Type: {animal["characteristics"]["type"]}")
+        print()
+
+def main():
+    animals_data = load_data(ANIMALS_FILE)
+    print_data(animals_data)
+
+
+
+if __name__ == "__main__":
+    main()
