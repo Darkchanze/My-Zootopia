de7f096b5e1a06bef7c482f325a3f8d6eb6c2dc5
Darkchanze <nico.wittemann@t-online.de>
2024-10-31 16:50:38 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,30 +1,46 @@
 import json
-from operator import ifloordiv
-from plistlib import loads
+
 
 ANIMALS_FILE = "animals_data.json"
+HTML_FILE = "animals_template.html"
 
-def load_data(ANIMALS_FILE):
-    with open(ANIMALS_FILE, "r") as handle:
+
+def load_json(FILE):
+    with open(FILE, "r") as handle:
         return json.load(handle)
 
 
-def print_data(animals_data):
+def read_html(FILE):
+    with open(FILE, "r") as handle:
+        return handle.read()
+
+
+def write_html(new_html):
+    with open("animals.html","w") as handle:
+        handle.write(new_html)
+
+
+def get_animal_string(animals_data):
+    animal_repository = ""
     for animal in animals_data:
         if "name" in animal:
-            print(f"Name: {animal["name"]}")
+            animal_repository += f"Name: {animal["name"]}\n"
         if "diet" in animal["characteristics"]:
-            print(f"Diet: {animal["characteristics"]["diet"]}")
+            animal_repository += f"Diet: {animal["characteristics"]["diet"]}\n"
         if "locations" in animal:
-            print(f"Location: {animal["locations"][0]}")
+            animal_repository += f"Location: {animal["locations"][0]}\n"
         if "type" in animal["characteristics"]:
-            print(f"Type: {animal["characteristics"]["type"]}")
-        print()
+            animal_repository += f"Type: {animal["characteristics"]["type"]}\n"
+        animal_repository += "\n"
+    return animal_repository
+
 
 def main():
-    animals_data = load_data(ANIMALS_FILE)
-    print_data(animals_data)
-
+    animals_data = load_json(ANIMALS_FILE)
+    animals_string = get_animal_string(animals_data)
+    html = read_html(HTML_FILE)
+    new_html = html.replace("__REPLACE_ANIMALS_INFO__", animals_string)
+    write_html(new_html)
 
 
 if __name__ == "__main__":
